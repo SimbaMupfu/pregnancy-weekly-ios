7d3feaf50c4e420a777b9024980424a9eab3847e
simba_mupfu <scmupfururirwa@gmail.com>
2023-12-23 06:41:31 +0200

    Add Struct to hold Week details

---
+++ Pregnancy Weekly/Models/WeekDetails.swift
@@ -0,0 +1,18 @@
+//
+//  WeekDetails.swift
+//  Pregnancy Weekly
+//
+//  Created by Simbarashe Mupfururirwa on 2023/12/23.
+//
+
+import Foundation
+
+struct WeekDetails {
+    var weekNumber: Int
+    var weekName: String
+    
+    init(weekNumber: Int, weekName: String) {
+        self.weekNumber = weekNumber
+        self.weekName = weekName
+    }
+}
